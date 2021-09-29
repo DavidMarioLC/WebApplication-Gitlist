c734e860e8cda02dc1a48c7a906172e4b385b15b
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-09-28 22:39:31 -0500

    [ADD] repositories

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import "./sass/main.scss";
 
 
 function App() {
+ 
+  const img:string = "https://avatars.githubusercontent.com/u/1500684?v=4";
   const fullname:string = 'Kent C. Dodds';
   const username:string = 'kentcdodds';
-  const biography:string = 'working hard to make the world better with software';
+  const biography:string = 'Destructuring React & the Static Web';
   const followers:string = '20.8';
   const following:string ='42';
   const star:string = '81';
@@ -14,14 +16,14 @@ function App() {
 
   return (
   <>
-  <header></header>
-    <main className="home">
-    <div className="sidebar">
+    
+    <div className="home">
+    <aside className="sidebar">
       <div className="profile">
         <div className="profile__user">
           <div className="avatar">
             <figure className="avatar__image">
-              <img src="https://avatars.githubusercontent.com/u/1500684?v=4" alt="username" title="username"  loading="lazy" />
+              <img src={img} alt="username" title="username" width="80" height="80"  loading="lazy" />
             </figure>
             <div className="avatar__info">
             <p className="avatar__fullname">{fullname}</p>
@@ -29,42 +31,79 @@ function App() {
             </div>
           </div>
         </div>
+        <div className="profile__buttons">
+              <button className="btn btn--text">Follow</button> 
+        </div>
         <div className="profile__details">
           <p className="biography">{biography}</p>
            <div className="info">
-              <a className="info__link" href="#l1"><i className="icon-users"></i> <span>{followers}k</span>  Followers</a> ·  
-              <a className="info__link" href="#l2"><span>{following}</span> Following</a> ·
-              <a className="info__link" href="#l3"><i className="icon-star"></i> <span>{star}</span></a>
+              <a className="info__link" href="#followers">
+                <i className="icon-users"></i>
+                <span className="info__value">{followers}k</span>
+                Followers
+              </a>  
+              <a className="info__link" href="#Following">
+                <span className="info__value">{following}</span>
+                Following
+                </a> 
+              <a className="info__link" href="#stars">
+                <i className="icon-star"></i>
+                <span className="info__value">{star}</span>
+              </a>
            </div>
         </div>
         <div className="profile__contact">
-          <ul className="contacts">
-            <li><a className="contacts__link" href="#c1"><i className="icon-pingMap"></i> {city}</a></li>
-            <li><a className="contacts__link" href="#c2"><i className="icon-link"></i> {website}</a></li>
-            <li><a className="contacts__link" href="#c3"><i className="icon-twitter"></i> {twitter}</a></li>
-          </ul>
-        </div>
-        <div className="profile__buttons">
-              <button className="btn btn--text">Follow</button>
-              {/* <button className="btn btn--icon"><i className="icon-heart"></i> Sponsor</button> */}
+          <div className="contacts">
+            <p className="contacts__item"><i className="icon-pingMap"></i> <span>{city}</span></p>
+            <p className="contacts__item" ><i className="icon-link"></i> <a className="contacts__link" href="#c2">{website}</a></p>
+            <p className="contacts__item" ><i className="icon-twitter"></i> <a className="contacts__link" href="#c3">{twitter}</a></p>
+          </div>
         </div>
       </div>
-     </div>
-  
-     <div className="main">
+     </aside>
+     <main className="main">
         <div className="actions">
           <div className="search">
-            
+            <input placeholder="Find a repository…" className="search__input" type="text" />
           </div>
           <div className="selectGroup">
-              
+          <button className="btn btn--text">Type <i className="icon-dropDown"></i></button>
+          <button className="btn btn--text">Language <i className="icon-dropDown"></i></button>
+          <button className="btn btn--text">Sort <i className="icon-dropDown"></i></button>
+          div
           </div>
         </div>
         <div className="repositories">
-  
+          <article className="repository">
+              <div className="repository__content">
+               <div className="repository__header">
+                 <a className="repository__name" href="#">testing-react-apps</a>
+                 <span className="repository__tag">Public</span>
+                </div>
+                
+                <p className="repository__description">A workshop for testing react applications</p>
+                <div className="repository__tags">
+                  <a className="tag" href="https://github.com/">css</a>
+                  <a className="tag" href="https://github.com/">html</a>
+                  <a className="tag" href="https://github.com/">javascript</a>
+                  <a className="tag" href="https://github.com/">react</a>
+                  <a className="tag" href="https://github.com/">sass</a>
+                </div>
+                <div className="repository__details">
+                  <span>Javascript</span>
+                  <time>5 days ago</time>
+                </div>
+              </div>  
+
+              <div className="repository__actions">
+                <button className="btn btn--star"><i className="icon-star"></i> Star</button>
+              </div>
+              
+          </article>
+          
         </div>
-     </div>
-   </main>
+     </main>
+   </div>
     
   </>
   
